2720670e41796d705fbbc35480df149fa2c21682
Aman Pandey <pandey.aman111@gmail.com>
2021-08-15 00:23:06 +0530

    feat:  function to hide & display remark block

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
+// Variables, querying the HTML DOM elements
+
 const billAmount = document.querySelector(".input-bill-amount");
 const cashReceived = document.querySelector(".input-payment-amount");
 const btnCount = document.querySelector(".btn-count");
+const btnNext = document.querySelector(".btn-next");
+const paymentBlock = document.querySelector(".div-payment");
+const changeBlock = document.querySelector(".div-change");
+const remarks = document.querySelector(".span-remark");
+const tableDiv = document.querySelector(".div-table");
+const imgDishes = document.querySelector(".img-dishes");
 
+/**Function to count the number of notes to be returned
+   and to print the same on the change table*/
 
+const nextCashDiv = ()=>{
+    if(billAmount.value == 0)
+        alert("No bill amount entered");
+    else{
+        btnNext.style.display = "none";
+        paymentBlock.style.display = "block";
+    }
+    
+}   
 
 
 const countNotes = () =>{
     let arrFinal;
-    if(billAmount.value > cashReceived.value)
-        alert("Cash not sufficient");
+    if(Number(billAmount.value) > Number(cashReceived.value)){
+        // console.log(billAmount.value +""+ cashReceived.value);
+        // console.log(Number(billAmount.value) > Number(cashReceived.value));
+        // alert("Cash not sufficient");
+        remarks.style.display = "block";
+        tableDiv.style.display = "none";
+        changeBlock.style.display = "block";
+        remarks.textContent = "CASH NOT SUFFICIENT";
+        imgDishes.style.display = "block";
+        
+    }
+    else if(billAmount.value == cashReceived.value){
+        remarks.style.display = "block";
+        tableDiv.style.display = "none";
+        changeBlock.style.display = "block";
+        imgDishes.style.display = "none";
+        remarks.textContent = "NO CHANGE TO RETURN";
+    }   
     else{
         let balance = cashReceived.value - billAmount.value;
         let arrInitial = [2000,500,100,50,20,10,5,2,1];
@@ -27,12 +62,16 @@ const countNotes = () =>{
             let noteString = ".td-note-"+index;
             let noteCountText = document.querySelector(noteString);
             noteCountText.textContent = number;
-            console.log(noteCountText)
+            // console.log(noteCountText)
         });
-        }
-
-       
+        changeBlock.style.display = "block";
+        tableDiv.style.display = "block";
+        remarks.style.display = "none";
+        imgDishes.style.display = "none";
+        } 
 }
 
 
-btnCount.addEventListener('click',countNotes)
+btnCount.addEventListener('click',countNotes);
+btnNext.addEventListener('click',nextCashDiv);
+
